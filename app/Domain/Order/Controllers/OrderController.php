abd8fa8eca38ce75710bd855ece12db97d1e5b5e
Dhik <mohammad18018@mail.unpad.ac.id>
2024-12-21 11:29:26 +0700

    update

--- app/Domain/Order/Controllers/OrderController.php
+++ app/Domain/Order/Controllers/OrderController.php
@@ -300,10 +300,10 @@ class OrderController extends Controller
             'x-api-key' => 'f5c80067e1da48e0b2b124558f5c533f1fda9fea72aa4a2a866c6a15a1a31ca8'
         ];
         $statuses = ['paid', 'process', 'pick', 'packing', 'packed', 'sent', 'completed'];
-        // $startDate = Carbon::now()->subDays(1)->format('Y-m-d');
-        // $endDate = Carbon::now()->format('Y-m-d');
-        $startDate = '2024-12-11';
-        $endDate = '2024-12-12';
+        $startDate = Carbon::now()->subDays(2)->format('Y-m-d');
+        $endDate = Carbon::now()->format('Y-m-d');
+        // $startDate = '2024-12-11';
+        // $endDate = '2024-12-12';
         $allOrders = [];
 
         foreach ($statuses as $status) {
